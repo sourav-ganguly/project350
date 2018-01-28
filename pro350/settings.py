7db2aca45ec7c8d94897a5e4f4314afbf09141f2
saadakaalo <souravganguly.bd@gmail.com>
2018-01-28 13:13:48 +0600

    started login_registration app

--- pro350/settings.py
+++ pro350/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'add_content.apps.AddContentConfig',
+    'login_registration.apps.LoginRegistrationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
